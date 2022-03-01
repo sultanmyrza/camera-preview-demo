a803ee0bc837ce77b4e8f8562a31e8c47589cfe7
sultanmyrza <sultanmyrza@gmail.com>
2022-03-01 11:38:51 +0800

    add ui elements for testing camera-preview plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  start() {
+    // read more at https://github.com/capacitor-community/camera-preview#startoptions
+  }
+  stop() {
+    // read more at https://github.com/capacitor-community/camera-preview#stop
+  }
 
-  constructor() {}
+  flip() {
+    // read more at https://github.com/capacitor-community/camera-preview#flip
+  }
 
+  captureSample() {
+    // read more at https://github.com/capacitor-community/camera-preview#capturesampleoptions
+  }
+
+  getSupportedFlashModes() {
+    // read more at https://github.com/capacitor-community/camera-preview#getsupportedflashmodes
+  }
+
+  setFlashMode() {
+    // read more at https://github.com/capacitor-community/camera-preview#setflashmodeoptions
+  }
+
+  startRecordVideo() {
+    // read more at https://github.com/capacitor-community/camera-preview#startrecordvideooptions-------android-only
+    // Note: according to docs it's Android only feature
+  }
+
+  stopRecordVideo() {
+    // read more at https://github.com/capacitor-community/camera-preview#stoprecordvideo-------android-only
+    // Note: according to docs it's Android only feature
+  }
+
+  setOpacity() {
+    // read more at https://github.com/capacitor-community/camera-preview#setopacityoptions-cameraopacityoptions-promise-------android-only
+    // Note: according to docs it's Android only feature
+  }
 }
