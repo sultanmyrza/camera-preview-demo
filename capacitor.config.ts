d9071c5d4d0c79008e9d4a20306776e1f33e17ff
sultanmyrza <sultanmyrza@gmail.com>
2022-03-01 11:13:51 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'camera-preview-demo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
